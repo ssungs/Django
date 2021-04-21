722b5645eecb939970d20de8761b87011c8cb330
ssungs <asdf4651@naver.com>
2021-04-21 16:28:35 +0900

    Practice django

--- session/firsr_django/first_server/views.py
+++ session/firsr_django/first_server/views.py
@@ -23,11 +23,18 @@ def verify(request):
 
 
 def countStr(request):
-    
-    countstr = {
-        'input_string': '{}'.format(request.POST['countstr']),
-        'count_all_string': len(request.POST['countstr']),
-        'count_string': len(''.join(request.POST['countstr'].split(' '))),
-        'count_paragraph': len(request.POST['countstr'].split(' '))
-    }
+    if len(request.POST['countstr']) != 0 :
+        countstr = {
+            'input_string': '{}'.format(request.POST['countstr']),
+            'count_all_string': len(request.POST['countstr']),
+            'count_string': len(''.join(request.POST['countstr'].split(' '))),
+            'count_paragraph': len(request.POST['countstr'].split(' '))
+        }
+    else:
+        countstr = {
+            'input_string': '글자를 입력하지 않았습니다.',
+            'count_all_string': '글자를 입력하지 않았습니다.',
+            'count_string': '글자를 입력하지 않았습니다.',
+            'count_paragraph': '글자를 입력하지 않았습니다.'
+        }
     return render(request, 'count.html', countstr)
